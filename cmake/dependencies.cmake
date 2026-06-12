b90975820650a73d2c3e74195b9372a2f12f192f
Jakub Neruda <nerudaj@email.cz>
2026-06-12 17:12:40 +0200

    CMake cleanup

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,8 +6,6 @@ set ( FSM_LIB_VERSION "main" )
 set ( CATCH2_VERSION "v3.8.0" )
 set ( ENTT_VERSION "3.16.0" )
 
-set ( ENABLE_LEGACY_ANIMATION OFF )
-
 CPMAddPackage("gh:nerudaj/dgm-engine-core-lib#${DGM_ENGINE_CORE_LIB_VERSION}")
 CPMAddPackage("gh:jarro2783/cxxopts#v${CXXOPTS_VERSION}")
 CPMAddPackage("gh:nlohmann/json#v${NLOHMANN_VERSION}")
