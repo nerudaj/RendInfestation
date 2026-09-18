cca843c4a2b54738e2d232795435de5f33ebb4d2
Jakub Neruda <nerudaj@email.cz>
2026-09-18 20:29:31 +0200

    Sounds normalized to same peak level

--- lib/include/audio/SoundId.hpp
+++ lib/include/audio/SoundId.hpp
@@ -4,21 +4,21 @@ namespace SoundId
 {
     using IdType = const char*;
 
-    const IdType Error = "error.wav";
-    const IdType Click = "click.wav";
-    const IdType Land = "land.wav";
-    const IdType Bullet = "bullet.wav";
-    const IdType Explosion = "rocket_explosion.wav";
-    const IdType Shotgun = "shotgun.wav";
-    const IdType ShrapnelFire = "grenade_launcher_fire.wav";
-    const IdType RocketLauncher = "zombie_cards_launcher.wav";
-    const IdType ScuttlebugAttack = "TY_SLICE.wav";
-    const IdType BigheadAttack = "infestation_bighead_attack.wav";
-    const IdType BigheadRangedAttack = "error.wav";
-    const IdType BehemothAttack = "infestation_behemoth_attack.wav";
-    const IdType ScuttlebugDeath = "infestation_scuttlebug_death.wav";
-    const IdType BigheadDeath = "zombiecards_zombie_growl2.wav";
-    const IdType BeholderDeath = "blood_spatter.wav";
-    const IdType Crafting = "crafting.wav";
-    const IdType TreeDestroyed = "error.wav";
+    const IdType Error = "error-2.wav";
+    const IdType Click = "click-2.wav";
+    const IdType Land = "land-2.wav";
+    const IdType Bullet = "bullet-2.wav";
+    const IdType Explosion = "rocket_explosion-2.wav";
+    const IdType Shotgun = "shotgun-2.wav";
+    const IdType ShrapnelFire = "grenade_launcher_fire-2.wav";
+    const IdType RocketLauncher = "zombie_cards_launcher-2.wav";
+    const IdType ScuttlebugAttack = "TY_SLICE-2.wav";
+    const IdType BigheadAttack = "infestation_bighead_attack-2.wav";
+    const IdType BigheadRangedAttack = "error-2.wav";
+    const IdType BehemothAttack = "infestation_behemoth_attack-2.wav";
+    const IdType ScuttlebugDeath = "infestation_scuttlebug_death-2.wav";
+    const IdType BigheadDeath = "zombiecards_zombie_growl2-2.wav";
+    const IdType BeholderDeath = "blood_spatter-2.wav";
+    const IdType Crafting = "crafting-2.wav";
+    const IdType TreeDestroyed = "error-2.wav";
 } // namespace SoundId
